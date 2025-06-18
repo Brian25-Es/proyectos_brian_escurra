43e1b75781bd0950b69ce601e2a6c5c0b50f5338
Matias <ramirezmatias1122@gmail.com>
2025-06-18 16:49:55 -0300

    segundo commit

---
+++ ejecicio2.1.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="estilos.css">
+    <title>Respuesta</title>
+    <?php
+    function obtenerColorFondo($anios) {
+        if (is_numeric($anios)) {
+            $anios = (int)$anios;
+            if ($anios <= 12) {
+                return "lightblue";
+            } elseif ($anios >= 13 && $anios <= 19) {
+                return "lightgreen";
+            } else {
+                return "lightgray";
+            }
+        }
+        return "white";
+    }
+
+    function descuento($anios){
+        $desc=0;
+        $precio=100;
+        if($anios<20){
+            $desc=$precio*(10/100);
+            $precio=$precio-$desc;
+        }
+        elseif($anios>50){
+            $desc=$precio*(5/100);
+            $precio=$precio-$desc;
+        }
+        return $precio;
+    }
+
+
+
+    $backgroundColor = "white";
+    if (isset($_REQUEST["anios"])) {
+        $backgroundColor = obtenerColorFondo($_REQUEST["anios"]);
+    }
+    ?>
+    <style>
+        body {
+            background-color: <?php echo $backgroundColor; ?>;
+        }
+    </style>
+</head>
+<body>
+<?php
+if (isset($_REQUEST["nombre"]) && isset($_REQUEST["anios"])) {
+    $nombre = $_REQUEST["nombre"];
+    $anios = $_REQUEST["anios"];
+
+    echo "<p>Nombre recibido: $nombre</p>";
+    echo "<p>Años recibidos: $anios</p>";
+
+    if (is_numeric($anios)) {
+        $anios = (int)$anios;
+        if ($anios > 12 && $anios < 20) {
+            echo "<p>Usted $nombre es un adolescente.</p>";
+        } else {
+            echo "<p>Usted $nombre no es un adolescente.</p>";
+        }
+    } else {
+        echo "<p>Por favor, ingrese un número válido en años.</p>";
+    }
+} else {
+    echo "<p>No se recibieron los datos.</p>";
+}
+
+$funciondescuento=descuento($anios);
+echo "<p>el precio de su entrada es: $funciondescuento</p>";
+?>
+</body>
+</html>
