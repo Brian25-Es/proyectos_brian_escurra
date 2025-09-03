0cfbe219af32723758685049d669792458212660
Brian25-Es <escurrabrian552@gmail.com>
2025-09-03 16:03:46 -0300

    0

--- BASEDEDATOSTP7.php
+++ BASEDEDATOSTP7.php
@@ -4,14 +4,11 @@ $username = "adminphp";
 $password = "TuContraseñaSegura";
 $dbname = "myDB";
 
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// sql to create table
 $sql = "CREATE TABLE EJERCICIO7 (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 nombre VARCHAR(30) NOT NULL,
